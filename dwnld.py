68d3a47bc4b5ba817714b61e20cb05647b182b00
Mir Nusrath Ali <nusrathali207@gmail.com>
2025-05-31 10:43:31 +0530

    Initial

---
+++ dwnld.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("nih-chest-xrays/data")
+
+print("/Users/mn.ali9/Documents/Generative-AI-for-Annotated-Medical-Imaging/data/raw/x-ray", path)
